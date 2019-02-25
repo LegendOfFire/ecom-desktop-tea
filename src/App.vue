ee7b6280e48b2d374dddeff0004399028c9f30e4
Frank FENG L <frank.l.feng@ericsson.com>
2019-02-25 12:21:39 +0800

    Add electron-builder vue@cli plug-in.

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <StartUp msg="Welcome to ECom Desktop Teaching Application"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StartUp from './components/StartUp.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    StartUp
   }
 }
 </script>
